da38939ae3de5daefea71fa85929bead0b5ebb4c
kamaraju1 <borrakamaraju123@gmail.com>
2022-12-18 11:35:18 +0000

    changedininport

--- names/import.js
+++ names/import.js
@@ -1,3 +1,3 @@
-const list = require("/home/workspace/coding-practices/coding-practice-2c/country/state/city/index.js");
-const namefunction = require("./index.js");
-console.log(namefunction(list));
+const list = require("../country/state/city/index.js");
+const getpeopleincity = require("/home/workspace/coding-practices/coding-practice-2c/names/index.js");
+console.log(getpeopleincity(list));
--- names/index.js
+++ names/index.js
@@ -1,3 +1,2 @@
 const getPeopleInCity = require("/home/workspace/coding-practices/coding-practice-2c/utilities/utils/index.js");
-const list = require("/home/workspace/coding-practices/coding-practice-2c/country/state/city/index.js");
 module.exports = getPeopleInCity;
