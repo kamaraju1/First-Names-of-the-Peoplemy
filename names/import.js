0e04cb7b45815515ba7833ecd0fba67df2410e04
kamaraju1 <borrakamaraju123@gmail.com>
2022-12-18 11:18:58 +0000

    namesoflist

---
+++ names/import.js
@@ -0,0 +1,3 @@
+const list = require("/home/workspace/coding-practices/coding-practice-2c/country/state/city/index.js");
+const namefunction = require("./index.js");
+console.log(namefunction(list));
